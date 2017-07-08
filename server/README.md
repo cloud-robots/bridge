3e3ead729e4730387e5b25d034e1e4447cb318eb
Juan Medina <jamedina@gmail.com>
2017-07-08 10:48:55 +0100

    using H2 console

--- server/README.md
+++ server/README.md
@@ -7,3 +7,15 @@ Bridge Server to run in any host
 ```shell
 $ java -jar cloud-robots.bridge.server-0.0.1-SNAPSHOT.jar
 ```
+
+The server use a in memory H2 database that can be accessed thought the following url:
+
+[http://localhost:8080/h2-console](http://localhost:8080/h2-console)
+
+The default values to use are:
+- Driver Class : org.h2.Driver
+- JDBC URL: jdbc:h2:mem:serverDB
+- User Name: sa
+- Password: [blank]
+
+This can be change in the application.yml
--- server/src/main/kotlin/cloud/robots/bridge/server/application/ServerConfig.kt
+++ server/src/main/kotlin/cloud/robots/bridge/server/application/ServerConfig.kt
@@ -7,19 +7,23 @@ import cloud.robots.bridge.server.jpa.entity.Topic
 import cloud.robots.bridge.server.jpa.repository.SubscribersRepository
 import cloud.robots.bridge.server.service.SubscriberService
 import cloud.robots.bridge.server.service.SubscriberServiceImpl
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import org.springframework.transaction.annotation.EnableTransactionManagement
 import javax.sql.DataSource
+
+
 
 
 @Suppress("unused")
 @Configuration
 @EnableJpaRepositories(basePackageClasses = arrayOf(SubscribersRepository::class))
+@EnableTransactionManagement
 class ServerConfig {
 
   @Bean
@@ -29,9 +33,8 @@ class ServerConfig {
   fun serverControllerAdvice() = ServerControllerAdvice()
 
   @Bean
-  fun serverDataSource(): DataSource = EmbeddedDatabaseBuilder()
-      .setType(EmbeddedDatabaseType.HSQL)
-      .build()
+  @ConfigurationProperties(prefix="bridge.server.datasource.serverDB")
+  fun serverDataSource(): DataSource = DataSourceBuilder.create().build()
 
   @Bean
   fun entityManagerFactory(
